101db44d195519b7375d23c4eb1915996715a903
ravi kakumanu <ravik@ravis-MacBook-Pro-2.local>
2025-10-27 22:29:40 +0000

    added config entries

--- src/main/java/uk/gov/hmcts/cp/cdk/clients/progression/ProgressionClientConfig.java
+++ src/main/java/uk/gov/hmcts/cp/cdk/clients/progression/ProgressionClientConfig.java
@@ -1,6 +1,9 @@
 package uk.gov.hmcts.cp.cdk.clients.progression;
 
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
 
 @ConfigurationProperties(prefix = "cdk.client.progression")
 public record ProgressionClientConfig(
---
+++ src/main/java/uk/gov/hmcts/cp/cdk/clients/progression/ProgressionConfig.java
@@ -0,0 +1,8 @@
+package uk.gov.hmcts.cp.cdk.clients.progression;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(ProgressionClientConfig.class)
+public class ProgressionConfig {}
