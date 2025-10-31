b172ca74e8bbad081ab5f1b20dbbf6356b0650ae
Sri Durga <sridurga@Sris-Mac-Studio.local>
2025-10-31 14:32:17 +0000

    PMD fixes

--- src/main/java/uk/gov/hmcts/cp/cdk/batch/clients/rag/RagClientException.java
+++ src/main/java/uk/gov/hmcts/cp/cdk/batch/clients/rag/RagClientException.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.cp.cdk.batch.clients.rag;
 
 public class RagClientException extends RuntimeException {
-    public RagClientException(String message, Throwable cause) {
+    public RagClientException(final String message, final Throwable cause) {
         super(message, cause);
     }
 }
