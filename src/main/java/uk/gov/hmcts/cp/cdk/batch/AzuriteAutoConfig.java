e042e592a440b9ef40324448b48893e72553d41e
satishkumarhmcts <satish.kumar1@HMCTS.NET>
2025-11-05 10:07:49 +0000

    pmd fixes

--- src/main/java/uk/gov/hmcts/cp/cdk/batch/AzuriteAutoConfig.java
+++ src/main/java/uk/gov/hmcts/cp/cdk/batch/AzuriteAutoConfig.java
@@ -2,7 +2,9 @@ package uk.gov.hmcts.cp.cdk.batch;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 import uk.gov.hmcts.cp.cdk.batch.storage.StorageProperties;
@@ -13,32 +15,38 @@ import java.time.Duration;
 @ConditionalOnProperty(name = "storage.connection-string", havingValue = "auto-azurite")
 public class AzuriteAutoConfig {
 
+    private static final String DEFAULT_IMAGE = "mcr.microsoft.com/azure-storage/azurite:3.33.0";
+    private static final int AZURITE_PORT = 10_000;
+    private static final String DEV_ACCOUNT_NAME = "devstoreaccount1";
+    private static final String DEV_ACCOUNT_KEY = "Eby8vdM02xNOcqFeqCnf2w==";
+
     @Bean(destroyMethod = "stop")
     public GenericContainer<?> azuriteContainer(
-            @Value("${storage.azurite.image:mcr.microsoft.com/azure-storage/azurite:3.33.0}") String image) {
-        GenericContainer<?> c = new GenericContainer<>(DockerImageName.parse(image))
-                .withExposedPorts(10000)
-                .withCommand("azurite-blob --loose --blobHost 0.0.0.0 --blobPort 10000")
+            @Value("${storage.azurite.image:" + DEFAULT_IMAGE + "}") final String image) {
+        final GenericContainer<?> azuriteContainer = new GenericContainer<>(DockerImageName.parse(image))
+                .withExposedPorts(AZURITE_PORT)
+                .withCommand("azurite-blob --loose --blobHost 0.0.0.0 --blobPort " + AZURITE_PORT)
                 .withStartupTimeout(Duration.ofSeconds(60));
-        c.start();
-        return c;
+        azuriteContainer.start();
+        return azuriteContainer;
     }
 
     @Bean
     @Primary
     public StorageProperties azuriteStorageProperties(
-            GenericContainer<?> azuriteContainer,
-            @Value("${storage.container:cdk}") String container,
-            @Value("${storage.copyPollIntervalMs:1000}") Long copyPollIntervalMs,
-            @Value("${storage.copyTimeoutSeconds:120}") Long copyTimeoutSeconds) {
+            final GenericContainer<?> azuriteContainer,
+            @Value("${storage.container:cdk}") final String container,
+            @Value("${storage.copyPollIntervalMs:1000}") final Long copyPollIntervalMs,
+            @Value("${storage.copyTimeoutSeconds:120}") final Long copyTimeoutSeconds) {
 
-        int mapped = azuriteContainer.getMappedPort(10000);
-        String host = azuriteContainer.getHost();
-        String blobEndpoint = "http://" + host + ":" + mapped + "/devstoreaccount1";
-        String connectionString =
+        final int mappedPort = azuriteContainer.getMappedPort(AZURITE_PORT);
+        final String host = azuriteContainer.getHost();
+        final String blobEndpoint = "http://" + host + ":" + mappedPort + "/" + DEV_ACCOUNT_NAME;
+
+        final String connectionString =
                 "DefaultEndpointsProtocol=http;" +
-                        "AccountName=devstoreaccount1;" +
-                        "AccountKey=Eby8vdM02xNOcqFeqCnf2w==;" +
+                        "AccountName=" + DEV_ACCOUNT_NAME + ";" +
+                        "AccountKey=" + DEV_ACCOUNT_KEY + ";" +
                         "BlobEndpoint=" + blobEndpoint + ";";
 
         return new StorageProperties(connectionString, container, copyPollIntervalMs, copyTimeoutSeconds);
