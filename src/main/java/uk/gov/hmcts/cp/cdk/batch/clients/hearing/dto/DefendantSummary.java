144e90e81d83746155521d85a302aea2d9590546
ravi kakumanu <ravik@ravis-MacBook-Pro-2.local>
2025-11-16 05:44:48 +0000

    fixing issue related to multi defendants

---
+++ src/main/java/uk/gov/hmcts/cp/cdk/batch/clients/hearing/dto/DefendantSummary.java
@@ -0,0 +1,7 @@
+package uk.gov.hmcts.cp.cdk.batch.clients.hearing.dto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@SuppressWarnings("PMD.ShortVariable")
+public record DefendantSummary(
+        @JsonProperty("id") String id
+) {}
