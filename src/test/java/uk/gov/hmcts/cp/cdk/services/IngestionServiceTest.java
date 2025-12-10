8196fdb89398ff508070a39e00800950ecbde29f
Natraj Molala <natraj.molala3@hmcts.net>
2025-12-10 11:00:52 +0000

    improve test coverage

--- src/test/java/uk/gov/hmcts/cp/cdk/services/IngestionServiceTest.java
+++ src/test/java/uk/gov/hmcts/cp/cdk/services/IngestionServiceTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.cp.cdk.services;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -21,32 +22,40 @@ import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.batch.core.job.Job;
 import org.springframework.batch.core.job.JobExecution;
 import org.springframework.batch.core.job.parameters.JobParameters;
 import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.core.task.SyncTaskExecutor;
 
 @DisplayName("Ingestion Service tests (asynchronous start)")
+@ExtendWith(MockitoExtension.class)
 class IngestionServiceTest {
+
+    @Mock
+    private JobExecution mockedExecution;
+    @Mock
+    private IngestionStatusViewRepository repo;
+    @Captor
+    private ArgumentCaptor<JobParameters> paramsCaptor;
+
+    @Mock
+    private JobOperator operator;
+    @Mock
+    private Job job;
 
     @Test
     @DisplayName("submits job asynchronously and returns STARTED with requestId")
     void launchesAsyncAndReturnsStarted() throws Exception {
         // given
         final String cppuid = "u-123";
-
-        final JobOperator operator = mock(JobOperator.class);
-        final Job job = mock(Job.class);
-
-        final JobExecution mockedExecution = mock(JobExecution.class);
-        when(mockedExecution.getId()).thenReturn(321L);
-
-        final ArgumentCaptor<JobParameters> paramsCaptor = ArgumentCaptor.forClass(JobParameters.class);
         when(operator.start(eq(job), paramsCaptor.capture())).thenReturn(mockedExecution);
-
-        final IngestionStatusViewRepository repo = mock(IngestionStatusViewRepository.class);
 
         // Use a synchronous executor so the async Runnable executes immediately in the test thread
         final var syncExecutor = new SyncTaskExecutor();
@@ -76,14 +85,30 @@ class IngestionServiceTest {
         assertThat(resp.getMessage()).contains("requestId=");
     }
 
+    @Test
+    @DisplayName("do nothing when job submitted asynchronously and fails")
+    void doNothing_whenJobExecutionThrowsException() throws Exception {
+        // given
+        final String cppuid = "u-123";
+        final IngestionService svc = new IngestionService(repo, operator, job, new SyncTaskExecutor());
+
+        final IngestionProcessRequest req = new IngestionProcessRequest();
+        req.setCourtCentreId(UUID.fromString("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"));
+        req.setRoomId(UUID.fromString("bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb"));
+        req.setDate(LocalDate.parse("2025-10-01"));
+
+        doThrow(JobRestartException.class).when(operator).start(eq(job), any(JobParameters.class));
+
+        // when
+        svc.startIngestionProcess(cppuid, req);
+
+        // then (params used for the async start)
+        verify(operator, times(1)).start(eq(job), any(JobParameters.class));
+    }
 
     @Test
     @DisplayName("getStatus valid row found for the caseId")
     void getStatus() {
-        final JobOperator operator = mock(JobOperator.class);
-        final Job job = mock(Job.class);
-        final IngestionStatusViewRepository repo = mock(IngestionStatusViewRepository.class);
-
         final IngestionService service = new IngestionService(repo, operator, job, new SyncTaskExecutor());
         final UUID caseId = UUID.randomUUID();
         final OffsetDateTime lastUpdated = OffsetDateTime.now();
