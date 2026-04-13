168440acd72e6e0ed918604c2e7d1a1152301a53
Natraj Molala <natraj.molala3@hmcts.net>
2026-04-13 12:05:13 +0100

    script to stub wiremock and test ingestion workflow

--- src/integrationTest/java/uk/gov/hmcts/cp/cdk/stub/DocumentIngestionInitiationApiStub.java
+++ src/integrationTest/java/uk/gov/hmcts/cp/cdk/stub/DocumentIngestionInitiationApiStub.java
@@ -1,24 +1,35 @@
 package uk.gov.hmcts.cp.cdk.stub;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static jakarta.json.Json.createObjectBuilder;
 import static java.util.UUID.randomUUID;
 import static org.apache.http.HttpStatus.SC_ACCEPTED;
+import static org.slf4j.LoggerFactory.getLogger;
 import static uk.gov.hmcts.cp.cdk.http.AzureSasUtil.generateSasUrl;
 
 import jakarta.json.JsonObject;
+import org.slf4j.Logger;
 
 public class DocumentIngestionInitiationApiStub {
 
+    private static final Logger LOGGER = getLogger(DocumentIngestionInitiationApiStub.class);
     private static final String INITIATE_DOCUMENT_UPLOAD = "/document-upload";
     public static final String APPLICATION_JSON = "application/json";
+
+    public static void setupStubs() {
+        configureFor("localhost", 8089);
+        stubInitiateDocumentUpload("documents-new", "destination" + randomUUID() + ".pdf");
+    }
 
     public static void stubInitiateDocumentUpload(final String containerName, final String blobName) {
 
         final String sasStorageUrl = generateSasUrl(containerName, blobName);
+        LOGGER.info("Stub:: sasStorageUrl: {}", sasStorageUrl);
+
         final JsonObject responseJson = createObjectBuilder()
                 .add("storageUrl", sasStorageUrl)
                 .add("documentReference", randomUUID().toString())
@@ -32,4 +43,7 @@ public class DocumentIngestionInitiationApiStub {
                 ));
     }
 
+    public static void main(String[] args) {
+        setupStubs();
+    }
 }
